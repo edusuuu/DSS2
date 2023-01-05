def69ad15191029af3491d8ff55e14639885492b
Dilany08 <dannelramos0802@gmail.com>
2023-01-05 13:10:02 +0800

    about us

--- app/src/main/java/com/example/dss/MainPage.java
+++ app/src/main/java/com/example/dss/MainPage.java
@@ -1,33 +1,33 @@
 package com.example.dss;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 public class MainPage extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_page);
+        @Override
+        protected void onCreate (Bundle savedInstanceState){
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main_page);
 
-        Button btnFast = findViewById(R.id.btnFast);
-        Button btnResto = findViewById(R.id.btnResto);
+            Button btnFast = findViewById(R.id.btnFast);
+            Button btnResto = findViewById(R.id.btnResto);
 
-        btnFast.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                setContentView(R.layout.activity_fast_food);
-            }
-        });
-        btnResto.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                setContentView(R.layout.activity_restaurant);
-            }
-        });
+            btnFast.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+
+                    setContentView(R.layout.activity_fast_food);
+                }
+            });
+            btnResto.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+
+                    setContentView(R.layout.activity_restaurant);
+                }
+            });
+        }
     }
-}
