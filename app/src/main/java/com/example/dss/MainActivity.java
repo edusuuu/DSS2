f763a926f299f024460ae7e0ca0f74b98db04ee7
edusuuu <edzeljohnangeles@gmail.com>
2023-01-05 13:57:40 +0800

    Scrollview sa mainpage

--- app/src/main/java/com/example/dss/MainActivity.java
+++ app/src/main/java/com/example/dss/MainActivity.java
@@ -18,7 +18,7 @@ public class MainActivity extends AppCompatActivity {
         btnStart.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                setContentView(R.layout.activity_main_page);
+                setContentView(R.layout.activity_fast_food);
             }
         });
     }
