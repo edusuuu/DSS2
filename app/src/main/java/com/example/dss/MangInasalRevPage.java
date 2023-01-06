28487e1daf46b61b5c582950dec7b92adbd11b93
merrylizapabiton <mtpabiton@bpsu.edu.ph>
2023-01-07 03:36:46 +0800

    mangin

---
+++ app/src/main/java/com/example/dss/MangInasalRevPage.java
@@ -0,0 +1,14 @@
+package com.example.dss;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MangInasalRevPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mang_inasal_rev_page);
+    }
+}
