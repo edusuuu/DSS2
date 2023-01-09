0282e2412ad5825e65bf87b06830f410530cd845
edusuuu <edzeljohnangeles@gmail.com>
2023-01-10 03:07:19 +0800

    Added Functionality for fastfoods

--- app/src/main/java/com/example/dss/FastFood.java
+++ app/src/main/java/com/example/dss/FastFood.java
@@ -7,6 +7,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class FastFood extends AppCompatActivity {
 
@@ -16,6 +18,121 @@ public class FastFood extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fast_food);
 
+        ImageView jollibee = findViewById(R.id.jollibee);
+        ImageView chowking = findViewById(R.id.chowking);
+        ImageView mcdo = findViewById(R.id.mcdo);
+        ImageView kfc = findViewById(R.id.kfc);
+        ImageView greenwich = findViewById(R.id.greenwich);
+        ImageView bgking = findViewById(R.id.burgerking);
+        ImageView mangina = findViewById(R.id.manginasal);
+        ImageView scuisine = findViewById(R.id.scuisinee);
+
+        jollibee.setClickable(true);
+        chowking.setClickable(true);
+        mcdo.setClickable(true);
+        kfc.setClickable(true);
+        greenwich.setClickable(true);
+        bgking.setClickable(true);
+        mangina.setClickable(true);
+        scuisine.setClickable(true);
+
+        jollibee.bringToFront();
+        chowking.bringToFront();
+        mcdo.bringToFront();
+        kfc.bringToFront();
+        greenwich.bringToFront();
+        bgking.bringToFront();
+        mangina.bringToFront();
+        scuisine.bringToFront();
+
+
+        jollibee.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setContentView(R.layout.activity_jollibee_rev_page);
+                Intent intent = new Intent(FastFood.this , JollibeeRevPage.class);
+                Toast.makeText(FastFood.this, "Loading page...", Toast.LENGTH_SHORT).show();
+
+                startActivity(intent);
+            }
+        });
+
+        chowking.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setContentView(R.layout.activity_chowking);
+                Intent intent = new Intent(FastFood.this , Chowking.class);
+                Toast.makeText(FastFood.this, "Loading page...", Toast.LENGTH_SHORT).show();
+
+                startActivity(intent);
+            }
+        });
+
+        mcdo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setContentView(R.layout.activity_mcdo_rev_page);
+                Intent intent = new Intent(FastFood.this , McdoRevPage.class);
+                Toast.makeText(FastFood.this, "Loading page...", Toast.LENGTH_SHORT).show();
+
+                startActivity(intent);
+            }
+        });
+
+        kfc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setContentView(R.layout.activity_kfc);
+                Intent intent = new Intent(FastFood.this , KFC.class);
+                Toast.makeText(FastFood.this, "Loading page...", Toast.LENGTH_SHORT).show();
+
+                startActivity(intent);
+            }
+        });
+
+        greenwich.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setContentView(R.layout.activity_greenwich);
+                Intent intent = new Intent(FastFood.this , Greenwich.class);
+                Toast.makeText(FastFood.this, "Loading page...", Toast.LENGTH_SHORT).show();
+
+                startActivity(intent);
+            }
+        });
+
+        bgking.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setContentView(R.layout.activity_burger_king);
+                Intent intent = new Intent(FastFood.this , BurgerKing.class);
+                Toast.makeText(FastFood.this, "Loading page...", Toast.LENGTH_SHORT).show();
+
+                startActivity(intent);
+            }
+        });
+
+        mangina.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setContentView(R.layout.activity_mang_inasal_rev_page);
+                Intent intent = new Intent(FastFood.this , MangInasalRevPage.class);
+                Toast.makeText(FastFood.this, "Loading page...", Toast.LENGTH_SHORT).show();
+
+                startActivity(intent);
+            }
+        });
+
+        scuisine.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setContentView(R.layout.activity_susies_cuisine);
+                Intent intent = new Intent(FastFood.this , SusiesCuisine.class);
+                Toast.makeText(FastFood.this, "Loading page...", Toast.LENGTH_SHORT).show();
+
+                startActivity(intent);
+            }
+        });
 
     }
 }
