b253f1367cd5791c832f2b2146396db0de2fb7a8
Dilany08 <dannelramos0802@gmail.com>
2023-01-10 12:56:16 +0800

    tgif

---
+++ app/src/main/java/com/example/dss/TGIFridays.java
@@ -0,0 +1,14 @@
+package com.example.dss;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TGIFridays extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tgifridays);
+    }
+}
