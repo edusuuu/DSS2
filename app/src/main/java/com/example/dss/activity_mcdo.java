d9b6e049089bea71f40e5b164741bee6694f02b5
merrylizapabiton <mtpabiton@bpsu.edu.ph>
2023-01-10 22:53:24 +0800

    mcdoooooo

---
+++ app/src/main/java/com/example/dss/activity_mcdo.java
@@ -0,0 +1,34 @@
+package com.example.dss;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+public class activity_mcdo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mcdo);
+
+        ImageView back = findViewById(R.id.btnBack);
+
+        back.setClickable(true);
+
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                setContentView(R.layout.activity_restaurant);
+                Intent intent = new Intent(McdoRevPage.this , FastFood.class);
+                Toast.makeText(McdoRevPage.this, "Returning...", Toast.LENGTH_SHORT).show();
+
+                startActivity(intent);
+            }
+        });
+    }
+}
