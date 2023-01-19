bc08aa7dbf43585844839418ddef67df81138775
edusuuu <edzeljohnangeles@gmail.com>
2023-01-19 12:42:14 +0800

    fixed login page

--- app/src/main/java/com/example/dss/LogInPage.java
+++ app/src/main/java/com/example/dss/LogInPage.java
@@ -2,6 +2,7 @@ package com.example.dss;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -25,13 +26,20 @@ public class LogInPage extends AppCompatActivity {
         loginbtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (username.getText().toString().equals("admin") && Password.getText().toString().equals("admin")) {
-                    //correct
+                if (username.getText().toString().equalsIgnoreCase("admin")
+                        && Password.getText().toString().equalsIgnoreCase("admin")) {
                     Toast.makeText(LogInPage.this, "LOGIN SUCCESSFUL", Toast.LENGTH_SHORT).show();
-                } else
-                    Toast.makeText(LogInPage.this, "LOGIN FAILED!", Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(LogInPage.this, MainPage.class);
+                    startActivity(intent);
+                } else {
+                    username.setError("User not found.");
+                    Password.setError("Password incorrect.");
 
+
+
+                }
             }
+
         });
     }
 }
