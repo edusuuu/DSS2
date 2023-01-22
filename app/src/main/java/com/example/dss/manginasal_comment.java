b7a990eda381eb0067273d86a2c196c5f76ce477
Dilany08 <dannelramos0802@gmail.com>
2023-01-22 23:27:47 +0800

    back button

--- app/src/main/java/com/example/dss/manginasal_comment.java
+++ app/src/main/java/com/example/dss/manginasal_comment.java
@@ -2,11 +2,14 @@ package com.example.dss;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class manginasal_comment extends AppCompatActivity {
 
@@ -15,6 +18,8 @@ public class manginasal_comment extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_manginasal_comment);
 
+        ImageView back = findViewById(R.id.btnBack);
+        back.setClickable(true);
 
         Button submit = findViewById(R.id.button);
         EditText name,comment;
@@ -84,6 +89,15 @@ public class manginasal_comment extends AppCompatActivity {
         n17.setVisibility(View.INVISIBLE);
         cm17.setVisibility(View.INVISIBLE);
 
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(manginasal_comment.this, MangInasalRevPage.class);
+                Toast.makeText(manginasal_comment.this, "Returning...", Toast.LENGTH_SHORT).show();
+
+                startActivity(i);
+            }
+        });
 
 
         submit.setOnClickListener(new View.OnClickListener() {
