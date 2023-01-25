4bc5418e46be8a81dcc605fb33c2557555b40060
edusuuu <edzeljohnangeles@gmail.com>
2023-01-25 18:51:39 +0800

    about us fixed

--- app/src/main/java/com/example/dss/AboutUs.java
+++ app/src/main/java/com/example/dss/AboutUs.java
@@ -2,7 +2,11 @@ package com.example.dss;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class AboutUs extends AppCompatActivity {
     //add comment
@@ -11,5 +15,15 @@ public class AboutUs extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_us);
+
+        ImageView back = findViewById(R.id.btnBack);
+        back.setClickable(true);
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(AboutUs.this,MainPage.class);
+                startActivity(i);
+            }
+        });
     }
 }
