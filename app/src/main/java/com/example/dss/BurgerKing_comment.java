9b168f3e831b199f05b30558efebac86a11a65c2
edusuuu <edzeljohnangeles@gmail.com>
2023-01-25 18:16:45 +0800

    mas maayos na comment section

--- app/src/main/java/com/example/dss/BurgerKing_comment.java
+++ app/src/main/java/com/example/dss/BurgerKing_comment.java
@@ -62,138 +62,171 @@ public class BurgerKing_comment extends AppCompatActivity {
         cm16 = findViewById(R.id.cm16);
         cm17 = findViewById(R.id.cm17);
 
-        n5.setVisibility(View.INVISIBLE);
-        cm5.setVisibility(View.INVISIBLE);
-        n6.setVisibility(View.INVISIBLE);
-        cm6.setVisibility(View.INVISIBLE);
-        n7.setVisibility(View.INVISIBLE);
-        cm7.setVisibility(View.INVISIBLE);
-        n8.setVisibility(View.INVISIBLE);
-        cm8.setVisibility(View.INVISIBLE);
-        n9.setVisibility(View.INVISIBLE);
-        cm9.setVisibility(View.INVISIBLE);
-        n10.setVisibility(View.INVISIBLE);
-        cm10.setVisibility(View.INVISIBLE);
-        n11.setVisibility(View.INVISIBLE);
-        cm11.setVisibility(View.INVISIBLE);
-        n12.setVisibility(View.INVISIBLE);
-        cm12.setVisibility(View.INVISIBLE);
-        n13.setVisibility(View.INVISIBLE);
-        cm13.setVisibility(View.INVISIBLE);
-        n14.setVisibility(View.INVISIBLE);
-        cm14.setVisibility(View.INVISIBLE);
-        n15.setVisibility(View.INVISIBLE);
-        cm15.setVisibility(View.INVISIBLE);
-        n16.setVisibility(View.INVISIBLE);
-        cm16.setVisibility(View.INVISIBLE);
-        n17.setVisibility(View.INVISIBLE);
-        cm17.setVisibility(View.INVISIBLE);
+        n5.setVisibility(View.GONE);
+        cm5.setVisibility(View.GONE);
+        n6.setVisibility(View.GONE);
+        cm6.setVisibility(View.GONE);
+        n7.setVisibility(View.GONE);
+        cm7.setVisibility(View.GONE);
+        n8.setVisibility(View.GONE);
+        cm8.setVisibility(View.GONE);
+        n9.setVisibility(View.GONE);
+        cm9.setVisibility(View.GONE);
+        n10.setVisibility(View.GONE);
+        cm10.setVisibility(View.GONE);
+        n11.setVisibility(View.GONE);
+        cm11.setVisibility(View.GONE);
+        n12.setVisibility(View.GONE);
+        cm12.setVisibility(View.GONE);
+        n13.setVisibility(View.GONE);
+        cm13.setVisibility(View.GONE);
+        n14.setVisibility(View.GONE);
+        cm14.setVisibility(View.GONE);
+        n15.setVisibility(View.GONE);
+        cm15.setVisibility(View.GONE);
+        n16.setVisibility(View.GONE);
+        cm16.setVisibility(View.GONE);
+        n17.setVisibility(View.GONE);
+        cm17.setVisibility(View.GONE);
 
         name.setText(Username.getName());
 
         back.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i = new Intent(BurgerKing_comment.this, BurgerKing.class);
+                Intent i = new Intent(BurgerKing_comment.this, AmarahsCorner.class);
                 Toast.makeText(BurgerKing_comment.this, "Returning...", Toast.LENGTH_SHORT).show();
-
 
                 startActivity(i);
             }
         });
+
         submit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if (n1.getText().toString().isEmpty() && cm1.getText().toString().isEmpty()){
                     n1.setText(name.getText().toString());
                     cm1.setText(comment.getText().toString());
+                    Toast.makeText(BurgerKing_comment.this, "Comment added!", Toast.LENGTH_SHORT).show();
 
                 }
                 else if (n2.getText().toString().isEmpty() && cm2.getText().toString().isEmpty()){
                     n2.setText(name.getText().toString());
                     cm2.setText(comment.getText().toString());
+                    Toast.makeText(BurgerKing_comment.this, "Comment added!", Toast.LENGTH_SHORT).show();
+
                 }
                 else if (n3.getText().toString().isEmpty() && cm3.getText().toString().isEmpty()){
                     n3.setText(name.getText().toString());
                     cm3.setText(comment.getText().toString());
+                    Toast.makeText(BurgerKing_comment.this, "Comment added!", Toast.LENGTH_SHORT).show();
+
                 }
                 else if (n4.getText().toString().isEmpty() && cm4.getText().toString().isEmpty()){
                     n4.setText(name.getText().toString());
                     cm4.setText(comment.getText().toString());
+                    Toast.makeText(BurgerKing_comment.this, "Comment added!", Toast.LENGTH_SHORT).show();
+
                 }
                 else if (n5.getText().toString().isEmpty() && cm5.getText().toString().isEmpty()){
                     n5.setText(name.getText().toString());
                     cm5.setText(comment.getText().toString());
                     n5.setVisibility(View.VISIBLE);
                     cm5.setVisibility(View.VISIBLE);
+                    Toast.makeText(BurgerKing_comment.this, "Comment added!", Toast.LENGTH_SHORT).show();
+
                 }
                 else if (n6.getText().toString().isEmpty() && cm6.getText().toString().isEmpty()){
                     n6.setText(name.getText().toString());
                     cm6.setText(comment.getText().toString());
                     n6.setVisibility(View.VISIBLE);
                     cm6.setVisibility(View.VISIBLE);
+                    Toast.makeText(BurgerKing_comment.this, "Comment added!", Toast.LENGTH_SHORT).show();
+
                 }
                 else if (n7.getText().toString().isEmpty() && cm7.getText().toString().isEmpty()){
                     n7.setText(name.getText().toString());
                     cm7.setText(comment.getText().toString());
                     n7.setVisibility(View.VISIBLE);
                     cm7.setVisibility(View.VISIBLE);
+                    Toast.makeText(BurgerKing_comment.this, "Comment added!", Toast.LENGTH_SHORT).show();
+
                 }
                 else if (n8.getText().toString().isEmpty() && cm8.getText().toString().isEmpty()){
                     n8.setText(name.getText().toString());
                     cm8.setText(comment.getText().toString());
                     n8.setVisibility(View.VISIBLE);
                     cm8.setVisibility(View.VISIBLE);
+                    Toast.makeText(BurgerKing_comment.this, "Comment added!", Toast.LENGTH_SHORT).show();
+
                 }
                 else if (n9.getText().toString().isEmpty() && cm9.getText().toString().isEmpty()){
                     n9.setText(name.getText().toString());
                     cm9.setText(comment.getText().toString());
                     n9.setVisibility(View.VISIBLE);
                     cm9.setVisibility(View.VISIBLE);
+                    Toast.makeText(BurgerKing_comment.this, "Comment added!", Toast.LENGTH_SHORT).show();
+
                 }
                 else if (n10.getText().toString().isEmpty() && cm10.getText().toString().isEmpty()){
                     n10.setText(name.getText().toString());
                     cm10.setText(comment.getText().toString());
                     n10.setVisibility(View.VISIBLE);
                     cm10.setVisibility(View.VISIBLE);
+                    Toast.makeText(BurgerKing_comment.this, "Comment added!", Toast.LENGTH_SHORT).show();
+
                 }
                 else if (n11.getText().toString().isEmpty() && cm11.getText().toString().isEmpty()){
                     n11.setText(name.getText().toString());
                     cm11.setText(comment.getText().toString());
                     n11.setVisibility(View.VISIBLE);
                     cm11.setVisibility(View.VISIBLE);
+                    Toast.makeText(BurgerKing_comment.this, "Comment added!", Toast.LENGTH_SHORT).show();
+
                 }
                 else if (n12.getText().toString().isEmpty() && cm12.getText().toString().isEmpty()){
                     n12.setText(name.getText().toString());
                     cm12.setText(comment.getText().toString());
                     n12.setVisibility(View.VISIBLE);
                     cm12.setVisibility(View.VISIBLE);
+                    Toast.makeText(BurgerKing_comment.this, "Comment added!", Toast.LENGTH_SHORT).show();
+
                 }
                 else if (n13.getText().toString().isEmpty() && cm13.getText().toString().isEmpty()){
                     n13.setText(name.getText().toString());
                     cm13.setText(comment.getText().toString());
                     n13.setVisibility(View.VISIBLE);
                     cm13.setVisibility(View.VISIBLE);
+                    Toast.makeText(BurgerKing_comment.this, "Comment added!", Toast.LENGTH_SHORT).show();
+
                 } else if (n14.getText().toString().isEmpty() && cm14.getText().toString().isEmpty()){
                     n14.setText(name.getText().toString());
                     cm14.setText(comment.getText().toString());
                     n14.setVisibility(View.VISIBLE);
                     cm14.setVisibility(View.VISIBLE);
+                    Toast.makeText(BurgerKing_comment.this, "Comment added!", Toast.LENGTH_SHORT).show();
+
                 } else if (n15.getText().toString().isEmpty() && cm15.getText().toString().isEmpty()){
                     n15.setText(name.getText().toString());
                     cm15.setText(comment.getText().toString());
                     n15.setVisibility(View.VISIBLE);
                     cm15.setVisibility(View.VISIBLE);
+                    Toast.makeText(BurgerKing_comment.this, "Comment added!", Toast.LENGTH_SHORT).show();
+
                 } else if (n16.getText().toString().isEmpty() && cm16.getText().toString().isEmpty()){
                     n16.setText(name.getText().toString());
                     cm16.setText(comment.getText().toString());
                     n16.setVisibility(View.VISIBLE);
                     cm16.setVisibility(View.VISIBLE);
+                    Toast.makeText(BurgerKing_comment.this, "Comment added!", Toast.LENGTH_SHORT).show();
+
                 } else if (n17.getText().toString().isEmpty() && cm17.getText().toString().isEmpty()){
                     n17.setText(name.getText().toString());
                     cm17.setText(comment.getText().toString());
                     n17.setVisibility(View.VISIBLE);
                     cm17.setVisibility(View.VISIBLE);
+                    Toast.makeText(BurgerKing_comment.this, "Comment added!", Toast.LENGTH_SHORT).show();
+
                 }
 
             }
